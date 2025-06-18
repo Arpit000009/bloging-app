93adcb92759fa27b51e6512628658f0a8923548d
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-06-18 23:29:49 +0530

    login

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require('mongoose')
+
+const userRouter = require('./routes/user')
 
 const app = express();
 const PORT = 8000;
 
-app.set('view engine',ejs)
+mongoose.connect('mongodb://localhost:27017/blogify').then((e) =>console.log("mongodb Connected"))
+
+app.set('view engine',"ejs")
 app.set('views',path.resolve("./views"))
+
+app.use(express.urlencoded({extended: false}));
+
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.use('/user',userRouter)
 
 app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`));
