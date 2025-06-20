9d32c574de86e84dcafc08f0e5f93878a2b8a7ad
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-06-21 05:00:19 +0530

    profile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const Blog = require('./models/blog')
 
 const userRouter = require('./routes/user');
 const blogRoute = require('./routes/blog');
@@ -19,11 +20,13 @@ app.set('views',path.resolve("./views"))
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie('token'))
+app.use(express.static(path.resolve('./public')))
 
-
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{
+    const allBlogs = await Blog.find({})
     res.render("home",{
         user:req.user,
+        blogs: allBlogs,
     });
 });
 
